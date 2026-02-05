860fe2759093e1197059798d2f2d64a167180ca3
Claire <cameron.liam.yon@gmail.com>
2026-02-05 13:35:04 -0500

    added try/catch and switch case to ensure the user enters a valid number, loop is now functional

--- libraryBookManagement.java
+++ libraryBookManagement.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class libraryBookManagement {
     public static void main(String[] args) {
@@ -6,7 +7,7 @@ public class libraryBookManagement {
 
         String userInput;
         boolean systemActive = true;
-        int temp;
+        int userInt;
 
         System.out.println("Library Main System");
         System.out.println("___________________");
@@ -14,8 +15,34 @@ public class libraryBookManagement {
         while(systemActive) {
             System.out.println("Please Select an Option:\n1. Register a new book\n2. Display all books\n3. Display all available books\n4. Search books by author\n5. Check out a book\n6. Return a book\n7. Exit\n(Enter the number of the option you'd like to select)");
             userInput = keyedInput.nextLine();
-            temp = Integer.parseInt(userInput);
-            break;
+            try {
+                userInt = Integer.parseInt(userInput);
+            }
+            catch (Exception InvalidInteger) {
+                System.out.println("What you entered was not a valid option, please only enter a number between 1 and 7:");
+                continue;
+            }
+            switch(userInt) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    systemActive = false;
+                    System.out.println("Shutting Down, have a nice day :)");
+                    break;
+                default:
+                    System.out.println("That is not an available option, please input a number between 1 and 7");
+                    continue;
+            }
         }
 
         keyedInput.close();
