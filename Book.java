5f79a506aaaf684c02e1f05a7584f585b3ef75c8
Claire <cameron.liam.yon@gmail.com>
2026-02-05 13:22:00 -0500

    added commentation to book.java

--- Book.java
+++ Book.java
@@ -1,9 +1,11 @@
 public class Book {
+    // Variable Delcaration
     private String title;
     private String author;
     private String isbn;
     private boolean available;
 
+    // Constructor for Object
     public Book(String title, String author, String isbn, boolean available) {
         this.title = title;
         this.author = author;
@@ -11,11 +13,13 @@ public class Book {
         this.available = available;
     }
 
+    // "Get" methods to get the variables of the object
     public String getTitle() { return title; }
     public String getAuthor() { return author; }
     public String getIsbn() { return isbn; }
     public boolean isAvailable() {return available; }
 
+    // Display info method to display all information about a book
     public void displayInfo() {
         System.out.print("Book Information\n\nBook Title: " + getTitle() + "\nAuthor: " + getAuthor() + "\nISBN: " + getIsbn() + "\nIs available to check out: ");
         if(available == true) {
