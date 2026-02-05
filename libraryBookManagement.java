5fff3246865f8a2c56d549e01cd5fbb2f25d177b
Claire <cameron.liam.yon@gmail.com>
2026-02-05 14:03:21 -0500

    user can now add books to the system, added commentation and imported arraylists

--- libraryBookManagement.java
+++ libraryBookManagement.java
@@ -3,11 +3,22 @@ import java.util.ArrayList;
 
 public class libraryBookManagement {
     public static void main(String[] args) {
+        // Start Scanner
         Scanner keyedInput = new Scanner(System.in);
 
-        String userInput;
+        // Boolean used to run the main loop
         boolean systemActive = true;
+
+        // Two strings to allow the user to select an option
+        String userInput;
         int userInt;
+
+        // Short list of variables used when user is creating a new book
+        String title;
+        String author;
+        String isbn;
+
+        ArrayList<Book> books = new ArrayList<>();
 
         System.out.println("Library Main System");
         System.out.println("___________________");
@@ -23,19 +34,28 @@ public class libraryBookManagement {
                 continue;
             }
             switch(userInt) {
-                case 1:
+                case 1: // Adds a book to the library
+                    System.out.println("Enter the name of the book you would like to add:");
+                    title = keyedInput.nextLine();
+                    System.out.println("Enter the author of the book you are adding:");
+                    author = keyedInput.nextLine();
+                    System.out.println("Enter the ISBN of the book you are adding:");
+                    isbn = keyedInput.nextLine();
+                    Book newBook = new Book(title, author, isbn, true);
+                    books.add(newBook);
+                    System.out.println("Book added successfully!");
                     break;
-                case 2:
+                case 2: // Displays the titles of all books
                     break;
-                case 3:
+                case 3: // Displays the titles of all books marked as "available"
                     break;
-                case 4:
+                case 4: // Search for a book based on the author
                     break;
-                case 5:
+                case 5: // Checks out a book from the library (sets available to false)
                     break;
-                case 6:
+                case 6: // Returns a book to the library (sets available to true)
                     break;
-                case 7:
+                case 7: // Ends the Program
                     systemActive = false;
                     System.out.println("Shutting Down, have a nice day :)");
                     break;
