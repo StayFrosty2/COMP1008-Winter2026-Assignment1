b617a5e9370eba176fe32b822c9fca0a6dd59514
Claire <cameron.liam.yon@gmail.com>
2026-02-05 13:20:42 -0500

    moved book to it's own file, forgot to start a respository yesterday when i started this, commits start part way into the project

--- libraryBookManagement.java
+++ libraryBookManagement.java
@@ -1,34 +1,5 @@
 import java.util.Scanner;
 
-class Book {
-    private String title;
-    private String author;
-    private String isbn;
-    private boolean available;
-
-    public Book(String title, String author, String isbn, boolean available) {
-        this.title = title;
-        this.author = author;
-        this.isbn = isbn;
-        this.available = available;
-    }
-
-    public String getTitle() { return title; }
-    public String getAuthor() { return author; }
-    public String getIsbn() { return isbn; }
-    public boolean isAvailable() {return available; }
-
-    public void displayInfo() {
-        System.out.print("Book Information\n\nBook Title: " + getTitle() + "\nAuthor: " + getAuthor() + "\nISBN: " + getIsbn() + "\nIs available to check out: ");
-        if(available == true) {
-            System.out.println("Yes");
-        }
-        else {
-            System.out.println("No");
-        }
-    }
-}
-
 public class libraryBookManagement {
     public static void main(String[] args) {
         Scanner keyedInput = new Scanner(System.in);
